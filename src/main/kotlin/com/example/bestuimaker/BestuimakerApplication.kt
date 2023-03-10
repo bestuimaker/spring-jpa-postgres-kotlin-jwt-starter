7e4b7d253ac478f169091d691c0714bc1037de1a
bpandey <v-bpandey@expediagroup.com>
2023-03-10 22:13:39 +0530

    First commit of spring-boot starter

---
+++ src/main/kotlin/com/example/bestuimaker/BestuimakerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.bestuimaker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BestuimakerApplication
+
+fun main(args: Array<String>) {
+	runApplication<BestuimakerApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/bestuimaker/BestuimakerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.bestuimaker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BestuimakerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
